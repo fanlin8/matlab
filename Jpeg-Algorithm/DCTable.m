fb7ce697178a77e203ee62ee33d75f871da90c7f
Fan Lin <linfan0558@gmail.com>
2015-09-29 12:09:48 -0500

    Create DCTable.m

---
+++ Jpeg-Algorithm/DCTable.m
@@ -0,0 +1,8 @@
+function [ dcTable ] = DCTable( )
+% Table for luminance DC coefficients
+% Will be used in Coding
+
+dcTable = {'00', '010', '011', '100', '101', '110', '1110', ...
+              '11110', '111110', '1111110', '11111110', '111111110'};
+end
+
